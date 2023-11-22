f7a53455b6ce30b5b550534b6ce3fc954c425d61
ultraviolet10 <aritrac1998@hotmail.com>
2023-11-22 16:50:35 +0530

    feat: use resend for emails

--- src/external/sendOtp.js
+++ src/external/sendOtp.js
@@ -1,20 +1,28 @@
-import axios from "axios";
+import { Resend } from "resend";
+import { RESEND_ERROR_CODES_BY_KEY } from "resend/build/src/interfaces";
 
 const sendOtp = async (email, otp) => {
   try {
-    const awsApi = process.env.REACT_APP_AWS_API;
-    const response = await axios.post(awsApi, { toEmail: email, otp: otp });
+    const resend = new Resend(process.env.REACT_APP_RESEND_API);
 
-    if (response.status !== 200) {
-      throw new Error(`Failed to send OTP: ${response.status}`);
+    const emailSent = await resend.emails.send({
+      from: "onboarding@resend.dev", // @todo replace with team email id
+      to: email,
+      subject: "OTP for t3.money",
+      html: `<p>Here's your OTP for t3.money: <strong>${otp}</strong></p>`, // @todo decide on copy for email
+    });
+    if (emailSent.data.id) {
+      // store email ID in supabase?
+      return true;
+    } else if (emailSent.error) {
+      // handle error
+      if (emailSent.error.name === RESEND_ERROR_CODES_BY_KEY.rate_limit_exceeded) {
+        // try again in 5 seconds
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        return sendOtp(email, otp);
+      }
+      return false;
     }
-
-    // Check if MessageId is present in the response data
-    if (!response.data.MessageId) {
-      throw new Error("Failed to send OTP: No MessageId in response");
-    }
-
-    return true;
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
